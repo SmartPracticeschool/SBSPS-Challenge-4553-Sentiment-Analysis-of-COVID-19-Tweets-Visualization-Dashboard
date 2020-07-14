4130f7eff6ac1bee7fd1317a694f16755c631f6b
Ananya Aprameya <ananyaaprameya5@gmail.com>
2020-07-15 05:22:10 +0530

    formed links between URL and HTML pages
    updated functions in views.py
    updated path in builder/urls.py

--- builder/urls.py
+++ builder/urls.py
@@ -2,5 +2,18 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('',views.index)
+    path('', views.index),
+    path('blank/', views.blank),
+    path('contact/', views.contact),
+    path('crowd_pred/', views.crowd_pred),
+    path('crowd_prediction/', views.crowd_prediction),
+    path('fake_info/', views.fake_info),
+    path('fake_info_prediction/', views.fake_info_prediction),
+    path('general/', views.general),
+    path('location_analysis/', views.location_analysis),
+    path('login/', views.login),
+    path('profile/', views.profile),
+    path('thank_you/', views.thank_you),
+    path('widgets/', views.widgets),
+
 ]
--- builder/views.py
+++ builder/views.py
@@ -2,5 +2,43 @@ from django.shortcuts import render
 from django .http import HttpResponse
 # Create your views here.
 
+def blank(request):
+    return render(request,'blank.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def crowd_pred(request):
+    return render(request,'crowd-pred.html')
+
+def crowd_prediction(request):
+    return render(request,'crowd-prediction.html')
+
+def fake_info(request):
+    return render(request,'fake-info.html')
+
+def fake_info_prediction(request):
+    return render(request,'fake-info-prediction.html')
+
+def general(request):
+    return render(request,'general.html')
+
 def index(request):
     return render(request,'index.html')
+
+def location_analysis(request):
+    return render(request,'location-analysis.html')
+
+def login(request):
+    return render(request,'login.html')
+
+def profile(request):
+    return render(request,'profile.html')
+
+def thank_you(request):
+    return render(request,'thank-you.html')
+
+def widgets(request):
+    return render(request,'widgets.html')
+
+
