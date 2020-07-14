4c28305b4a6506b4161f5d828c8a99cf4e8a3503
Ananya Aprameya <ananyaaprameya5@gmail.com>
2020-07-15 03:55:20 +0530

    Tested the functioning and flow
    Flow and Function of the site is tested using a Hello World template

--- tweeysa/urls.py
+++ tweeysa/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('builder/' , include('builder.urls'))
+    path('' , include('builder.urls'))
 ]
